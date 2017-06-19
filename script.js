b69acc65a2d86db51c573ab716b966e309d7c5db
lorifritz <lfritz@holmanauto.com>
2017-06-19 18:40:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function init(){
     ul = document.getElementById(‘image_slider’);
     liItems = ul.children;
     imageNumber = liItems.length;
-    imageWidth = liItems[0].children[0].offsetWidth;
+    imageWidth = liItems[4].children[0].offsetWidth;
     // set ul’s width as the total width of all images in image slider.
     ul.style.width = parseInt(imageWidth * imageNumber) + ‘px’;
     slider(ul);
