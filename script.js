29189297364831c0236ade0bc9178245a3688b13
lorifritz <lfritz@holmanauto.com>
2017-06-19 22:16:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,1 @@
-var interval = 3500; // You can change this value to your desired speed. The value is in milliseconds, so if you want to advance a slide every 5 seconds, set this to 5000.
-var switching = setInterval("toggleSlide(true)", interval);
+
