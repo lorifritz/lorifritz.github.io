2f7d9e657268281b1d117b69ceea5b0ade87955a
lorifritz <lfritz@holmanauto.com>
2017-06-20 18:02:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
-<script type="text/javascript">
+
     $(function(){
         var scroller = $('#scroller div.innerScrollArea');
         var scrollerContent = scroller.children('ul');
