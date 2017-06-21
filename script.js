786ead1a0c04f7687efb3c990cdcfdfc8b7be636
lorifritz <lfritz@holmanauto.com>
2017-06-21 18:46:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -24,28 +24,4 @@ function rotateDiv(stor){
 
 
 
-function rotateDiv(stor){
-  var divs = document.getElementById("storyContainer").getElementsByTagName("div");
-  for (var i=0; i < divs.length; i++ ) {
-    var div = divs[i];
-    if ( (div.id != "")) {
-	if(i != stor){
-        	div.style.display = "none";
-	}
-	else{
-		div.style.display = "block";
-	}
-    }
-  }
-  
-    var spans = document.getElementById("nav").getElementsByTagName("span");
-  for (var i=0; i < spans.length; i++ ) {
-    var span = spans[i];
-    if ( (span.id != "")) {
-	if(i != stor)
-        	span.className = "none";
-	else
-		span.className = "selStory";
-    }
-  }
-}
+
