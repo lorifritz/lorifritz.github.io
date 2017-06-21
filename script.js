5c71b13aac9bc08cfecaeb6d14650dafcc016fbe
lorifritz <lfritz@holmanauto.com>
2017-06-21 18:37:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -30,3 +30,76 @@ function showNext(){
 
 	rotateDiv(stor);
 }
+function showNext(){
+	if(stor < maxstor)
+		stor++;
+	else
+		stor=0;
+
+	rotateDiv(stor);
+}
+
+function stoprot() {
+	clearTimeout(timeout);
+}
+
+
+function showNext(){
+	if(stor < maxstor)
+		stor++;
+	else
+		stor=0;
+
+	rotateDiv(stor);
+}
+
+function showPrev(){
+	if(stor > 0)
+		stor—;
+	else
+		stor=maxstor;
+
+	rotateDiv(stor);
+}
+
+function showStoryOne(){
+	stor=0;
+	rotateDiv(stor);
+}
+function showStoryTwo(){
+	stor=1;
+	rotateDiv(stor);
+}
+function showStoryThree(){
+	stor=2;
+	rotateDiv(stor);
+}
+function showStoryFour(){
+	stor=3;
+	rotateDiv(stor);
+}
+function rotateDiv(stor){
+  var divs = document.getElementById("storyContainer").getElementsByTagName("div");
+  for (var i=0; i < divs.length; i++ ) {
+    var div = divs[i];
+    if ( (div.id != "")) {
+	if(i != stor){
+        	div.style.display = "none";
+	}
+	else{
+		div.style.display = "block";
+	}
+    }
+  }
+  
+    var spans = document.getElementById("nav").getElementsByTagName("span");
+  for (var i=0; i < spans.length; i++ ) {
+    var span = spans[i];
+    if ( (span.id != "")) {
+	if(i != stor)
+        	span.className = "none";
+	else
+		span.className = "selStory";
+    }
+  }
+}
